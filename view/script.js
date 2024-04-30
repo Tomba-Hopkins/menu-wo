90cfe0d771b1dad43175bb4d833fbf6a844fe8fb
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-04-30 12:12:58 +0700

    makin kece ini gilak bet

--- view/script.js
+++ view/script.js
@@ -13,3 +13,32 @@ const sections = document.querySelectorAll('.section')
 sections.forEach((section) => {
     observer.observe(section)
 })
+
+
+
+// random quotes
+const quotes = [
+    `"The only way to do great work is to love what you do." ~Steve Jobs`,
+    `"Perang sejati adalah perang yang terjadi di dalam diri seseorang." ~Miyamoto Musashi`,
+    `"Don't count the days, make the days count." ~Muhammad Ali`,
+    `"The soul becomes dyed with the color of its thoughts." ~Marcus Aurelius`,
+    `"Hidup ini keras dan berat [terlanjur hidup jalani saja] ~Me"`
+]
+
+const tombol = document.querySelector('.button-quotes')
+const quote = document.querySelector('.quotes')
+
+
+tombol.addEventListener('click', function(){
+    const nilai = Math.floor(Math.random() * 5)
+    const isi = quotes[nilai]
+    quote.textContent = isi
+    quote.style.animation = 'ilang 1s linear'
+    quote.style.display = 'flex'
+
+    setTimeout(() => {
+        quote.style.animation = 'none'
+    }, 2000)
+
+
+})
