e01157def2c202aba11adbff0deb098a2a756411
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-06-03 11:55:35 +0700

    ada stopwatch coy

--- view/script.js
+++ view/script.js
@@ -47,10 +47,60 @@ tombol.addEventListener('click', function(){
 // Stopwatch
 const stopwatchTime = document.querySelector('p-time')
 const startButton = document.querySelector('.start')
+const stopButton = document.querySelector('.pause')
 
-startButton.add('click', () => {
+const pJam = document.querySelector('.p-h')
+const pMenit = document.querySelector('.p-m')
+const pDetik = document.querySelector('.p-s')
+
+let jam = 0
+let menit = 0
+let detik = 0
+let muter = null
+
+startButton.addEventListener('click', () => {
+    if (muter === null) {
+        muter = setInterval(() => {
+            detik += 1
+            pDetik.innerHTML = detik
+            
+            if (detik === 60 ){
+                detik = 0
+                pDetik.innerHTML = detik
+                menit += 1
+                pMenit.innerHTML = menit
+                
+                if (menit === 60 ) {
+                    menit = 0
+                    pMenit.innerHTML = menit
+                    jam += 1
+                    pJam.innerHTML = jam
+                }
+                
+            }
+            
+        }, 1000)
+    }
     
 })
+
+
+stopButton.addEventListener('click', () => {
+    if (muter !== null){
+        clearInterval(muter)
+        muter = null
+    }
+    detik = 0
+    menit = 0
+    jam = 0
+
+    pDetik.innerHTML = ""
+    pMenit.innerHTML = ""
+    pJam.innerHTML = ""
+})
+
+
+
 
 
 
